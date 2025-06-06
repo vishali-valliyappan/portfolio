75275792e6c860fe0e6de0a5bbb7b40b9883a87e
vishali-valliyappan <vishalivalliyappan9@gmail.com>
2025-06-06 12:39:07 +0800

    Change the Resume pdf

--- components/Header.jsx
+++ components/Header.jsx
@@ -37,7 +37,7 @@ const Header = () => {
           <Image src={assets.right_arrow_white} alt="" className=" w-4 " />
         </a>
         <a
-          href="/vishali.pdf"
+          href="/Vishali Resume May 2025.pdf"
           download
           className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2"
         >
